5864abfba843ddc86c599f4438b3d1c0df541d1b
Kabalan Elahmadie <kahmadieh@yahoo.com>
2019-05-15 07:04:25 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,8 +1,4 @@
 function basicTeenager(age) {
-
-}
-
-function teenager(age) {
   if (age >=13 && age <=19)
   {
      return "You are a teenager!";
@@ -11,17 +7,42 @@ function teenager(age) {
   {
     return "You are not a teenager!";
   }
+}
 
+function teenager(age) {
+  if (age >=13 && age <=19)
+  {
+     return "You are a teenager!";
+  }
+  else if ( age < 13 || age > 19)
+  {
+    return "You are not a teenager!";
+  }
 }
 
 function ageChecker(age) {
-
+  if (age >=13 && age <=19)
+  {
+     return true;
+  }
+  return false 
 }
-
 function ternaryTeenager(age) {
-
+    return (age >=13 && age <=19) ? "You are a teenager!" : "You are not a teenager!"  
 }
 
 function switchAge(age) {
+  switch(age) {
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+    case 19:
+      return "You are a teenager!"
+    default:
+      return "You are not a teenager!"
+  }
 
 }
